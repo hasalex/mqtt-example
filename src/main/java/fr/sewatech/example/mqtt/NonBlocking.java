9b1571c14e8b776647095dad546b8fc2ff652189
Alexis Hassler <alexis@sewatech.org>
2013-12-20 19:32:51 +0100

    Other MQTT example : non blocking client

--- src/main/java/fr/sewatech/example/mqtt/NonBlocking.java
+++ src/main/java/fr/sewatech/example/mqtt/NonBlocking.java
@@ -5,27 +5,24 @@ import org.eclipse.paho.client.mqttv3.*;
 /**
  * @author Alexis Hassler
  */
-public class Blocking {
+public class NonBlocking {
     public static void main(String[] args) throws MqttException {
         publish();
         receive();
     }
 
     private static void receive() throws MqttException {
-        final MqttClient client = new MqttClient("tcp://localhost:1883", "C2");
-        client.connect();
+        final MqttAsyncClient client = new MqttAsyncClient("tcp://localhost:1883", "C2");
         client.setCallback(new MqttCallback() {
             @Override
             public void connectionLost(Throwable throwable) {
                 System.out.println("Arggh, connection lost");
-                System.exit(0);
             }
 
             @Override
             public void messageArrived(String s, MqttMessage mqttMessage) throws Exception {
                 System.out.println("Hey, message arrived on topic " + s + " : " + new String(mqttMessage.getPayload(), "UTF-8"));
-                client.disconnect();
-                client.close();
+
             }
 
             @Override
@@ -33,12 +30,27 @@ public class Blocking {
                 System.out.println("Oh, delivery complete");
             }
         });
-        client.subscribe("sample");
+
+        client.connect(null, new IMqttActionListener() {
+            @Override
+            public void onSuccess(IMqttToken iMqttToken) {
+                try {
+                    client.subscribe("sample", 1);
+                } catch (MqttException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(IMqttToken iMqttToken, Throwable throwable) {
+                System.out.println("Sigh, not connected");
+            }
+        });
 
     }
 
     private static void publish() throws MqttException {
-        MqttClient client = new MqttClient("tcp://localhost:1883", "C1");
+        MqttClient client = new MqttClient("tcp://localhost:1883", "C1", null); // null for no client-side persistence
         client.connect();
         MqttTopic topic = client.getTopic("sample");
         MqttMessage message = new MqttMessage();
